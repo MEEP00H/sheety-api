53327567f9c9356d15ce65c287f64617129746f0
Pooh <sophon@punchup.world>
2021-02-10 15:35:45 +0700

    Get Data api

--- src/services/gsheet/gsheet.class.js
+++ src/services/gsheet/gsheet.class.js
@@ -1,36 +1,48 @@
 /* eslint-disable no-unused-vars */
+const { google } = require("googleapis");
+const keys = require("../../../keys.json");
+
 exports.Gsheet = class Gsheet {
-  constructor (options) {
+  constructor(options) {
     this.options = options || {};
   }
 
-  async find (params) {
+  async find(params) {
     return [];
   }
 
-  async get (id, params) {
-    return {
-      id, text: `A new message with ID: ${id}!`
-    };
+  async get(id, params) {
+    const client = new google.auth.JWT(
+      keys.client_email,
+      null,
+      keys.private_key,
+      ["https://www.googleapis.com/auth/spreadsheets"]
+    );
+    const gsapi = google.sheets({ version: "v4", auth: client });
+    const data = await gsapi.spreadsheets.get({
+      spreadsheetId: id,
+    });
+
+    return { data };
   }
 
-  async create (data, params) {
+  async create(data, params) {
     if (Array.isArray(data)) {
-      return Promise.all(data.map(current => this.create(current, params)));
+      return Promise.all(data.map((current) => this.create(current, params)));
     }
 
     return data;
   }
 
-  async update (id, data, params) {
+  async update(id, data, params) {
     return data;
   }
 
-  async patch (id, data, params) {
+  async patch(id, data, params) {
     return data;
   }
 
-  async remove (id, params) {
+  async remove(id, params) {
     return { id };
   }
 };
