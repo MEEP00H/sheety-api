22b29fd24177909939dfe526990a334971e8a978
Nattakarn Plianchan <nattakarn.plian@gmail.com>
2021-02-10 14:19:40 +0700

    fix get spreadsheet id from url

--- src/hooks/get-spreadsheet-id.js
+++ src/hooks/get-spreadsheet-id.js
@@ -5,15 +5,23 @@
 module.exports = (options = {}) => {
   return async context => {
     // console.log(context)
-    const {url} = context.data;
-  //get spreadsheetId which locate between "/d/" and "/edit"
-    var spreadsheetId = url.substring( 
-      url.lastIndexOf("/d/")+3, 
-      url.lastIndexOf("/edit"));
+    const { url } = context.data;
+    //get spreadsheetId which locate between "/d/" and "/edit"
+    var hasEdit = url.includes("/edit", 12);
+    if (hasEdit) {
+      var spreadsheetId = url.substring(
+        url.lastIndexOf("/d/") + 3,
+        url.lastIndexOf("/edit")
+      );
+    }
+    else {
+      var spreadsheetId = url.substring(
+        url.lastIndexOf("/d/") + 3);
+    }
     console.log(spreadsheetId);
     context.data = {
       ...context.data,
-      "spreadsheetId":spreadsheetId
+      "spreadsheetId": spreadsheetId
     }
     // console.log(context)
 
