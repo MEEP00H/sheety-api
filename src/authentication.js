e473bb9b71a7a657ea236fced9384306bb1b5c1e
Nattakarn Plianchan <nattakarn.plian@gmail.com>
2021-02-10 13:27:20 +0700

    no message

--- src/authentication.js
+++ src/authentication.js
@@ -1,12 +1,29 @@
 const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
 const { LocalStrategy } = require('@feathersjs/authentication-local');
 const { expressOauth } = require('@feathersjs/authentication-oauth');
+const axios = require('axios');
+const { OAuthStrategy } = require('@feathersjs/authentication-oauth');
 
+class GoogleStrategy extends OAuthStrategy {
+  async getEntityData(profile) {
+  
+    // this will set 'googleId'
+    const baseData = await super.getEntityData(profile);
+    console.log(baseData);
+    // this will grab the picture and email address of the Google profile
+    return {
+      ...baseData,
+      profilePicture: profile.picture,
+      email: profile.email
+    };
+  }
+}
 module.exports = app => {
   const authentication = new AuthenticationService(app);
 
   authentication.register('jwt', new JWTStrategy());
   authentication.register('local', new LocalStrategy());
+  authentication.register('google', new GoogleStrategy());
 
   app.use('/authentication', authentication);
   app.configure(expressOauth());
