adc1d670a8df492b9aa304c4973e0b06f9596783
Nattakarn Plianchan <nattakarn.plian@gmail.com>
2021-02-17 15:29:41 +0700

    delete all unnecessary in Project hook

--- src/services/projects/projects.hooks.js
+++ src/services/projects/projects.hooks.js
@@ -1,27 +1,9 @@
-const { authenticate } = require('@feathersjs/authentication').hooks;
-const getProjectOwner = require('../../hooks/get-project-owner');
-const { BadRequest } = require("@feathersjs/errors");
-const DataGoogleSheet = require("../../util/data-google-sheet");
-const _ = require("lodash");
-
-const check_Spreadsheed_Id =  () =>  async  context => {
-    const { spreadsheetId } = context.data;
-    const gsheet = await DataGoogleSheet(spreadsheetId);
-    // // console.log(context);
-    console.log("G-SHEET");
-    console.log(gsheet);
-    if (!_.has(context.result, "message")) {
-
-      };
-  };
-
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
- // create: [authenticate("jwt"), getProjectOwner(), getSpreadsheetId()],
-    create: [check_Spreadsheed_Id()], 
+    create: [], 
     update: [],
     patch: [],
     remove: []
